845b8b9aca11a49bbd561c1ab417334fb446245d
Andrew Park <acpark123@gmail.com>
2020-06-09 20:53:46 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,18 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  
+  
+  combined_string = ""
+  row_counter = 0
+    while row_counter < src.count do
+      element_counter = 0
+      while element_counter < src[row_counter].count do
+        element = src[row_counter][element_counter]
+        combined_string += "#{element} " if element.class == String
+        element_counter += 1  
+      end
+    row_counter += 1  
+    end
+  combined_string
 end
